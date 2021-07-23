fb904fd1acf5363dfc5725b75be935640d96d0ad
Muskanxworkz <87843782+Muskanxworkz@users.noreply.github.com>
2021-07-23 16:58:10 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,44 @@
+class Student{
+
+String name;
+String city;
+String ID;
+
+void developer(){
+System.out.println(city+" "+name+" "+ID+" developer at xzworkz!!!");
+}
+public static void main(String args[]){
+Student muskan= new Student();
+muskan.name="muskan";
+muskan.city="Hubli";
+muskan.ID="xz40";
+muskan.developer();
+
+Student sunita= new Student();
+sunita.name="sunita";
+sunita.city="Gadag";
+sunita.ID="xz41";
+sunita.developer();
+
+Student priya= new Student();
+priya.name="priya";
+priya.city="Bangalore";
+priya.ID="xz42";
+priya.developer();
+
+Student ruhi= new Student();
+ruhi.name="ruhi";
+ruhi.city="Bellary";
+ruhi.ID="xz43";
+ruhi.developer();
+
+Student jyoti= new Student();
+jyoti.name="jyoti";
+jyoti.city="Gadag";
+jyoti.ID="xz44";
+jyoti.developer();
+
+}
+}
+
+
